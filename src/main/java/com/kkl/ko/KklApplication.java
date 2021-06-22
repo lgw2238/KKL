ea9b57eed58e5cdd7d8b4517be1a160c76ed7cd0
lgw2238 <nihao3634@naver.com>
2021-06-22 23:08:26 +0900

    KKL FIRST COMMIT

---
+++ src/main/java/com/kkl/ko/KklApplication.java
@@ -0,0 +1,13 @@
+package com.kkl.ko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KklApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KklApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kkl/ko/KklApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkl.ko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KklApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
